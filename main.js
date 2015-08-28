05bd3faac431b30e8735d4c66de59b68fa269a10
Samer Buna <samer.buna@gmail.com>
2015-08-27 17:44:07 -0700

    Finish the MVP for the game

--- main.js
+++ main.js
@@ -1,25 +1,43 @@
 $(document).ready(function(){
+  var randomNumber = Math.floor(Math.random()*9);
+  var turnsRemaining = 3;
+  var $counter = $("#counter");
+  var gameStates = {
+    'on': 'Treasure Hunt',
+    'won': 'You won!',
+    'lost': 'Game Over'
+  };
+  var currentGameState = 'on';
 
-    var randomNumber = Math.floor(Math.random()*9);
-    console.log(randomNumber);
+  $counter.text(turnsRemaining);
+  var isGameStillOn = function() {
+    return currentGameState === 'on';
+  }
+  var changeGameState = function(newState) {
+    currentGameState = newState;
+    $("#headerMessage").text(gameStates[currentGameState]);
+  };
 
+  var handleClick = function(event){
+    if (isGameStillOn()) {
+      event.preventDefault();
+      decrementTurnsRemaining();
+      var $link = $(this);
+      var correctAnswer = $link.data("id") === randomNumber;
+      $link.addClass(correctAnswer ? "winner" : "loser");
+      if (correctAnswer) {
+        changeGameState('won');
+        return;
+      }
+    }
+    if (turnsRemaining === 0) {
+      changeGameState('lost');
+    }
+  };
 
+  $(".btn").on("click", handleClick);
 
-
-
-
-
-
-    $("btn").on("click", function(){
-
-        if(this.id === randomNumber) {
-          $(this).addclass("winner");
-        }
-        else {
-          $(this).addClass("loser");
-        }
-
-
-    });
-
+  var decrementTurnsRemaining = function() {
+    $counter.text(--turnsRemaining);
+  };
 });
